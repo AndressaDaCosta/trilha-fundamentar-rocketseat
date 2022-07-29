b6492f73d07b1b0acd1f9909aba27e69c6b019c7
Andressa Da Costa <andressadacostan@gmail.com>
2022-07-29 20:10:40 -0300

    Update function_1.js

--- 11-Guia Estelar JavaScript/10-Estrutura de repetição/function_1.js
+++ 11-Guia Estelar JavaScript/10-Estrutura de repetição/function_1.js
@@ -3,6 +3,8 @@
 // break - para a execução do loop
 // continue - pula a execução do momento
 
+// for(inicialização de uma variável; condição de continuação para o loop; expressão final)
+    
 for(let i = 1; i <= 10; i++){
     console.log(i);
 }

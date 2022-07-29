2f90fad84c027ecd05e813febf0595bb00c609b3
Andressa Da Costa <andressadacostan@gmail.com>
2022-07-29 19:56:31 -0300

    Update function_2.js

--- 11-Guia Estelar JavaScript/09-Condicionais e controle de fluxo/function_2.js
+++ 11-Guia Estelar JavaScript/09-Condicionais e controle de fluxo/function_2.js
@@ -10,6 +10,13 @@ if (true){
 
 }
 
+
+if (condição) {
+    //apenas será executado o bloco de código caso condição seja true
+} else {
+  // apenas será executado o bloco de código caso condição do if seja false
+}
+
 // aplicação, leitura de temperatura corporal
 
 let temperature = 35.5;
@@ -19,4 +26,17 @@ if(temperature >= 37.5){
     console.log("Febre moderada");
 } else{
     console.log("Saudável");
+}
+
+
+let temperature = 36.9
+let highTemperature = temperature >= 37.5
+let mediumTemperature = temperature < 37.5 && temperature >= 37
+
+if(highTemperature) {
+    console.log('Febre alta')
+} else if(mediumTemperature) {
+    console.log('Febre moderada')
+} else {
+    console.log('Saudável')
 }

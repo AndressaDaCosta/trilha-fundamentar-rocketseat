012c35e34e3dbe2e1b91c4fd773effeb2bb5ad51
Andressa Da Costa <andressadacostan@gmail.com>
2022-07-29 20:08:30 -0300

    Update function.js

--- 11-Guia Estelar JavaScript/09-Condicionais e controle de fluxo/function.js
+++ 11-Guia Estelar JavaScript/09-Condicionais e controle de fluxo/function.js
@@ -22,3 +22,28 @@ try {
 }
 
 console.log("após a função de erro");
+
+
+
+=====
+    
+    function sayMyName(name = '') {
+    if (name === '') {
+        throw 'Nome é obrigatório'
+       //  || OU throw new Error("Nome é necessario")
+    }
+
+    console.log(name)
+}
+Nesse caso, se o nome vier vazio, será disparada uma mensagem.
+
+Precisamos agora usar o try/catch para capturarmos esse erro, caso contrário, ele irá encerrar nossa aplicação, e nós o faremos da seguinte maneira:
+
+try {
+    sayMyName()
+} catch(e) {
+    console.log(e)
+}
+
+console.log('após ao try/catch')
+O try irá executar a função, enquanto o catch vai capturar o erro e atribuir à variável e, que só existe dentro do catch. Após isso, mesmo com o erro, nossa aplicação não será interrompida por completo.

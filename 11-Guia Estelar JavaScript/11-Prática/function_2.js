cec1d1388d590e1b7655d3efbe35dfc628c95e5b
Andressa Da Costa <andressadacostan@gmail.com>
2022-07-29 21:19:42 -0300

    Update function_2.js

--- 11-Guia Estelar JavaScript/11-Prática/function_2.js
+++ 11-Guia Estelar JavaScript/11-Prática/function_2.js
@@ -19,6 +19,67 @@ numérico para sistema de notas em caracteres tipo A B C
 
 */
 
+
+let score = 90
+
+if (score >= 90) {
+    console.log(score)
+    
+//     
+    
+    function getScore(score) {
+     let scoreA = score >= 90 && score <= 100
+     let scoreB = score >= 80 && score <= 89
+     let scoreC = score >= 70 && score <= 79
+     let scoreD = score >= 60 && score <= 69
+     let scoreF = score < 60 && score >= 0
+     
+     
+     let scoreFinal;
+        
+        if (scoreA {
+            scoreFinal = "A"
+        } else if (scoreB) {
+            scoreFinal = "B"    
+        }  else if (scoreC) {
+            scoreFinal = "C"    
+        }  else if (scoreD) {
+            scoreFinal = "D"    
+        }  else if (scoreF) {
+            scoreFinal = "F"    
+        }  else {
+            scoreFinal = "Nota inválida"
+        }          
+        
+        return scoreFinal
+    }
+    
+    console.log(getScore(101))
+    
+    console.log(getScore(-1))
+    
+    console.log(getScore(0))
+    
+    console.log(getScore(45))
+    
+    console.log(getScore(60))
+    
+    console.log(getScore(61))
+    
+    console.log(getScore(75))
+    
+    console.log(getScore(95))
+    
+    console.log(getScore())
+    
+    
+    
+    
+//     
+//     
+//     
+    
+
 function converteNotas(nota){
     let notaConvertida = 0;
     if(nota < 60 && nota >= 0){

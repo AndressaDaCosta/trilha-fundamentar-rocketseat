9fa948da43a6d5ac2f4dca650d51f5582ab6cb43
Andressa Da Costa <andressadacostan@gmail.com>
2022-08-01 16:11:11 -0300

    Update script.js

--- 15-Pilotando com a DOM/Eventos/Argumento-event/script.js
+++ 15-Pilotando com a DOM/Eventos/Argumento-event/script.js
@@ -2,6 +2,13 @@
 // argumento Event
 const input = document.querySelector('input')
 
-input.onkeypress = function() {
-    
+input.onkeydown = function(event) {
+    console.log(event.currentTarget.value)
+}
+input.onkeypress = function(event) {
+    console.log(event.key)
+}
+
+input.onclick = function(event) {
+    console.log(event)
 }
